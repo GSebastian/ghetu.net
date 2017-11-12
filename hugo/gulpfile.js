4ea74819e86c9b29b30e3bfb801df70d5d984ce9
Sebastian <sebastian@ghetu.net>
2017-11-12 00:19:41 +0000

    Integrated the landing page into hugo

--- hugo/gulpfile.js
+++ hugo/gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require("gulp"),
     path = require('path'),    
     shell = require('gulp-shell'),    
     LessAutoprefix = require('less-plugin-autoprefix'),
+    removeEmptyLines = require('gulp-remove-empty-lines'),
     autoprefix = new LessAutoprefix({ browsers: ['last 10 versions'] })
     
 // Compile LESS files to CSS
@@ -14,13 +15,25 @@ gulp.task("less", function () {
         .pipe(gulp.dest("./static/css"))
 })
 
+gulp.task("htmlClean", function () {
+    gulp.src('./public/*.html')
+    .pipe(removeEmptyLines({
+      removeComments: false
+    }))
+    .pipe(gulp.dest('./public/'));
+  });
+
+gulp.task("clearGenerated", shell.task(['rm -rf ./public']));
+
+gulp.task("buildHugo", shell.task(['hugo']));
+
 // Watch asset folder for changes
 gulp.task("watch", ["less"], function () {
     gulp.watch("src/less/**/*", ["less"])
 })
 
-gulp.task('firebase', ["less"], shell.task([
-    'rm -rf ./public && hugo && firebase deploy --token ***REMOVED***'
+gulp.task('firebase', ["less", "clearGenerated", "buildHugo", "htmlClean"], shell.task([
+    'firebase deploy --token ***REMOVED***'
 ]))
 
 // Set watch as default task
